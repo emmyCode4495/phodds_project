850e8c47eb4262386a971c8e5ab8506b2b1fd9ae
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-06-28 13:39:07 +0100

    added file

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App(){
         barStyle="light-content"
         backgroundColor= {colors.main}/>
         <RootNavigator />
-        {/* <HomeScreenCards/> */}
+        <HomeScreenCards/>
         {/* <VendorScreen/> */}
         </View>
         // </NavigationContainer>
